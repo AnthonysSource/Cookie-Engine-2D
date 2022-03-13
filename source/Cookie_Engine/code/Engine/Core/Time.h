8c59b16663e3a761591d3fa0422938cef8a444f0
Anthonys-source <87545402+AnthonysSource@users.noreply.github.com>
2022-03-13 14:19:35 +0100

    Setup app time data definition

---
+++ source/Cookie_Engine/code/Engine/Core/Time.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "BasicTypes.h"
+
+namespace Cookie {
+struct Time {
+
+	static f64 m_SecondsUpTime;
+	static f64 m_SecondsUpTimeLastUpdate;
+
+	static u64 m_TicksUptime;
+	static u64 m_TicksUptimeLastUpdate;
+
+	static f32 m_DeltaTime;
+};
+} // namespace Cookie
