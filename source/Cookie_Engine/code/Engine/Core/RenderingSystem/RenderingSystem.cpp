fb2d5f597d433d261faaf784dda881b5f8cf7670
Anthonys-source <87545402+AnthonysSource@users.noreply.github.com>
2022-03-26 17:55:32 +0100

    Move Rendering API to own folder

--- source/Cookie_Engine/code/Engine/Core/RenderingSystem/RenderingSystem.cpp
+++ source/Cookie_Engine/code/Engine/Core/RenderingSystem/RenderingSystem.cpp
@@ -1,6 +1,6 @@
 #include "RenderingSystem.h"
 #include "Core/Application.h"
-#include "Core/RenderingSystem/RenderingAPI.h"
+#include "Core/RenderingAPI/RenderingAPI.h"
 
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
