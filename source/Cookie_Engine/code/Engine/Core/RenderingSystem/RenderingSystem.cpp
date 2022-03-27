c3c9153a3bd8ec5f343286b71e977e3e1833e47e
Anthonys-source <87545402+AnthonysSource@users.noreply.github.com>
2022-03-27 16:12:00 +0200

    Control quad rotation with imgui

--- source/Cookie_Engine/code/Engine/Core/RenderingSystem/RenderingSystem.cpp
+++ source/Cookie_Engine/code/Engine/Core/RenderingSystem/RenderingSystem.cpp
@@ -24,6 +24,8 @@ namespace RenderingSystem {
 	IndexBuffer indexBuffer;
 	Program program;
 
+	glm::vec3 quadRot = glm::vec3(0.0f);
+
 	float vertices[] = {
 		-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.5f, 0.5f, 0.0f, -0.5f, 0.5f, 0.0f,
 	};
@@ -66,26 +68,27 @@ namespace RenderingSystem {
 		ImGui::NewFrame();
 
 		// Set MVP matrix
-		glm::mat4 view =
-			glm::lookAt(glm::vec3(0.0f, 0.0f, 5.0f), glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 5.0f), glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		glm::mat4 proj = glm::perspective(45.0f, 1280.0f / 720.0f, 0.1f, 1000.0f);
 		glm::mat4 model = glm::mat4(1.0f);
 
-		float rotRadians = (float)glm::radians(250.0f * glfwGetTime());
-		model = glm::rotate(model, rotRadians, glm::vec3(1.0f, 0.0f, 0.0f));
-		model = glm::rotate(model, rotRadians, glm::vec3(0.0f, 1.0f, 0.0f));
-		model = glm::rotate(model, rotRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::rotate(model, quadRot.x, glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, quadRot.y, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::rotate(model, quadRot.z, glm::vec3(0.0f, 0.0f, 1.0f));
 
 		program.SetUniformMat4("model", glm::value_ptr(model));
 		program.SetUniformMat4("view", glm::value_ptr(view));
 		program.SetUniformMat4("projection", glm::value_ptr(proj));
 
+		// IMGUI Rendering
+		ImGui::Begin("Quad Parameters");
+		ImGui::SliderFloat3("Rotation", glm::value_ptr(quadRot), -5.0f, 5.0f, "%.3f", 1.0f);
+		ImGui::End();
+
 		// Issue Drawcall
 		Context::BindProgram(&program);
 		Context::DrawIndexed(&vertexArray);
 
-		// IMGUI Rendering
-		ImGui::ShowDemoWindow();
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
