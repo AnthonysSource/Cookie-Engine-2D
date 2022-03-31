a509c11554685d1ee83fd2739d0910a529319fdb
Anthonys-source <87545402+AnthonysSource@users.noreply.github.com>
2022-03-31 19:10:41 +0200

    Import type_ptr in math.h

--- source/Cookie_Engine/code/Engine/Core/Math.h
+++ source/Cookie_Engine/code/Engine/Core/Math.h
@@ -1,8 +1,10 @@
 #pragma once
 #include "glm/gtc/quaternion.hpp"
 #include "glm/mat4x4.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-namespace Cookie{
-    using Vec3 = glm::vec3;
-    using Quat = glm::quat;
-}
+namespace Cookie {
+using Vec3 = glm::vec3;
+using Quat = glm::quat;
+} // namespace Cookie
