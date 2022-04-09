ce839f6bc28941f7aa39b3360631aa109a22e2b0
Anthonys-source <87545402+AnthonysSource@users.noreply.github.com>
2022-04-09 22:36:31 +0200

    Entity admin header first pass

--- source/Cookie_Engine/code/Engine/EntityObjectModel/EntityAdmin.h
+++ source/Cookie_Engine/code/Engine/EntityObjectModel/EntityAdmin.h
@@ -5,6 +5,9 @@
 #include "Core/Math.h"
 #include "Core/Random/Random.h"
 
+// Remove this in the future
+#include "Core/InputSystem/InputSystem.h"
+
 #include <stdlib.h>
 
 namespace Cookie {
@@ -26,16 +29,19 @@ namespace Object {
 		vec3 m_Up;
 	};
 
-	struct ScriptComponent{
-		
-	};
+	struct ScriptComponent {};
 
-	std::vector<TransformComponent>
-		m_Transforms;
+	std::vector<TransformComponent> m_Transforms;
+	CameraComponent m_Camera;
+	InputComponent m_Input;
 
 	const u32 ENTITIES_AMOUNT = 10;
 
 	void GenerateCookies() {
+		m_Camera.m_Forward = vec3(0.0f, 0.0f, 1.0f);
+		m_Camera.m_Position = vec3(0.0f, 0.0f, 0.0f);
+		m_Camera.m_Up = vec3(0.0f, 1.0f, 0.0f);
+
 		for (size_t i = 0; i < ENTITIES_AMOUNT; i++) {
 			TransformComponent t{};
 			t.m_Position = vec3(-5.0f + 1.0f * i, 0.0f, 0.0f);
@@ -51,5 +57,54 @@ namespace Object {
 	}
 
 	void ClearCookies() { m_Transforms.clear(); }
+
+	// ---------------------------------------------------
+	using EntityID = u32;
+	const EntityID MAX_ENTITIES = 5000;
+
+	using ComponentType = u8;
+	const ComponentType MAX_COMPONENTS = 128;
+
+	using Signature = std::bitset<MAX_COMPONENTS>;
+
+	class IComponentArray {
+	  public:
+		virtual ~IComponentArray() = default;
+		virtual void EntityDestroyed(EntityID entity) = 0;
+	};
+
+	class System {
+	  public:
+		virtual void Init(Signature signature);
+		virtual void Update(f32 dt);
+
+	  protected:
+		Signature m_Signature;
+	};
+
+	class EntityAdmin {
+	  public:
+		void CreateEntity();
+		void DestroyEntity(EntityID entity);
+
+		template <typename T> void AddComponent(EntityID entity, T component);
+		template <typename T> void RemoveComponent(EntityID entity);
+		template <typename T> T *GetComponent(EntityID entitiy);
+		template <typename T> ComponentType GetComponentType();
+
+		template <typename T> void RegisterSystem();
+
+	  private:
+		std::queue<EntityID> m_AvailableEntityIDs;
+		std::array<Signature, MAX_ENTITIES> m_Signatures;
+		u32 m_ActiveEntitiesCount;
+
+		std::unordered_map<const char *, ComponentType> m_ComponentTypes{};
+		std::unordered_map<const char *, IComponentArray *> m_ComponentArrays{};
+		ComponentType m_NextComponentType{};
+
+		std::unordered_map<const char *, System *> m_Systems{};
+	};
+
 } // namespace Object
 } // namespace Cookie
