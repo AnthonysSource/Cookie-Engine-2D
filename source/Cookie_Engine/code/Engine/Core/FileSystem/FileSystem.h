d6edee71ee387bd572543c9d02b7aeb10969b7c5
Anthonys-source <87545402+AnthonysSource@users.noreply.github.com>
2022-04-15 17:04:31 +0200

    Add future file system functions

--- source/Cookie_Engine/code/Engine/Core/FileSystem/FileSystem.h
+++ source/Cookie_Engine/code/Engine/Core/FileSystem/FileSystem.h
@@ -5,6 +5,12 @@
 namespace Cookie {
 namespace FileSystem {
 
+	// bool IsFile(const char *path);
+	// bool IsDirectory(const char *path);
+
+	// inline bool IsFile(const String *path) { return IsFile(path->c_str()); };
+	// inline bool IsDirectory(const String *path) { return IsDirectory(path->c_str()); };
+
 	void WriteToFile(const char *path, const char *data, const size_t size);
 	void ReadFile(const char *path, char *outputBuffer, const size_t size);
 
