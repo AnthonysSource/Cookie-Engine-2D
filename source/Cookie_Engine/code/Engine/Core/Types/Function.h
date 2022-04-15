291e67082ffd10cdf015549538d77e85a7e134db
Anthonys-source <87545402+AnthonysSource@users.noreply.github.com>
2022-04-15 13:21:16 +0200

    Move function out of pointers.h

---
+++ source/Cookie_Engine/code/Engine/Core/Types/Function.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Cookie {
+
+template <typename T> using Func = std::function<T>;
+
+}
--- source/Cookie_Engine/code/Engine/Core/Types/Pointers.h
+++ source/Cookie_Engine/code/Engine/Core/Types/Pointers.h
@@ -5,8 +5,4 @@ namespace Cookie {
 	template <typename T> using Ref = std::shared_ptr<T>;
 	template <typename T> using WeakRef = std::weak_ptr<T>;
 
-    // This isn't really a pointer but until we find a better
-    // place to put it we can define it here
-	template <typename T> using Func = std::function<T>;
-    
 } // namespace Cookie
