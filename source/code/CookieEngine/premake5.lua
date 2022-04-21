d2a238993bbac9da52012b6502137fa8ef47b996
Anthonys-source <87545402+AnthonysSource@users.noreply.github.com>
2022-04-21 18:15:53 +0200

    Refactor Premake scripts

--- source/code/CookieEngine/premake5.lua
+++ source/code/CookieEngine/premake5.lua
@@ -27,3 +27,19 @@ project "CookieEngine"
 group "ThirdParty"
     include("ThirdParty/glfw")
 group "" -- end of "ThirdParty"
+
+-- Function to be used by external libraries that want to use this project
+-- This is to workaround the fact that Premake doesn't have public includes
+-- like CMake
+function UseCookieEngine()
+    includedirs { 
+        "%{wks.location}/source/code/CookieEngine/ThirdParty/glfw/include",
+        "%{wks.location}/source/code/CookieEngine/ThirdParty/glad/include",
+        "%{wks.location}/source/code/CookieEngine/ThirdParty/glm",
+        "%{wks.location}/source/code/CookieEngine/ThirdParty/imgui",
+        "%{wks.location}/source/code/CookieEngine/ThirdParty/stb_image",
+        "%{wks.location}/source/code/CookieEngine/ThirdParty/spdlog/include",
+        "%{wks.location}/source/code/CookieEngine/Engine"
+    }
+    links { "CookieEngine" }
+ end
--- source/code/CookieGame/premake5.lua
+++ source/code/CookieGame/premake5.lua
@@ -4,20 +4,12 @@ project "CookieGame"
         "**.cpp",
         "**.h"
     }
-    includedirs {
-        "%{wks.location}/source/code/CookieEngine/ThirdParty/glfw/include",
-        "%{wks.location}/source/code/CookieEngine/ThirdParty/glad/include",
-        "%{wks.location}/source/code/CookieEngine/ThirdParty/glm",
-        "%{wks.location}/source/code/CookieEngine/ThirdParty/imgui",
-        "%{wks.location}/source/code/CookieEngine/ThirdParty/stb_image",
-        "%{wks.location}/source/code/CookieEngine/ThirdParty/spdlog/include",
-        "%{wks.location}/source/code/CookieEngine/Engine"
-    }
     links {
-        "CookieEngine",
         "opengl32.lib",
         "GLFW"
     }
+
+    UseCookieEngine();
 
     postbuildcommands {
         "{COPYDIR} %{wks.location}/source/resources/** %{cfg.targetdir}"
