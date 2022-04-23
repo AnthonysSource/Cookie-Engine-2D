c9ddcb2744935731c5d886839bab99e517b15770
Anthonys-source <87545402+AnthonysSource@users.noreply.github.com>
2022-04-23 10:25:28 +0200

    Update Input Sys State Logging

--- source/code/CookieEngine/Engine/Core/InputSystem/InputSystem.cpp
+++ source/code/CookieEngine/Engine/Core/InputSystem/InputSystem.cpp
@@ -31,12 +31,14 @@ namespace Cookie {
 			// Process input events
 			for (i32 i = 0; i < s_InputEventsBuffer.size(); i++) {
 				InputEvent e = s_InputEventsBuffer.front();
-
-				LogInput(e);
+				
 				// Logging to inspect system state
+				//LogInput(e);
+
+				s_InputEventsBuffer.pop();
 			}
 		}
-		
+
 		void InputSystem::Shutdown() {}
 
 		void WindowKeyEventHandle(GLFWwindow *window, int key, int scancode, int action, int mods) {
@@ -61,8 +63,8 @@ namespace Cookie {
 		void InputSystem::Init(WindowData *window) { glfwSetKeyCallback(window->m_Handle, WindowKeyEventHandle); }
 
 		void LogInput(InputEvent e) {
-			CKE_LOG_INFO("Event Data -> Key Name: {} / Keycode: {} / Scancode : {} / Action: {} / Mod: {}",
-						 glfwGetKeyName(e.m_KeyCode, e.m_ScanCode), e.m_KeyCode, e.m_ScanCode, e.m_Action, e.m_Mods);
+			// CKE_LOG_INFO("Event Data -> Key Name: {} / Keycode: {} / Scancode : {} / Action: {} / Mod: {}",
+			//			 glfwGetKeyName(e.m_KeyCode, e.m_ScanCode), e.m_KeyCode, e.m_ScanCode, e.m_Action, e.m_Mods);
 
 			// if (e.m_Action == 0) {
 			// 	CKE_LOG_INFO("[{}] Released", glfwGetKeyName(e.m_KeyCode, e.m_ScanCode));
@@ -70,11 +72,9 @@ namespace Cookie {
 			// 	CKE_LOG_INFO("[{}] Pressed", glfwGetKeyName(e.m_KeyCode, e.m_ScanCode));
 			// }
 
-			CKE_LOG_INFO("Input State Data -> Key Name: {} / Event Action: {} / Keydown: {} / Keyheld: "
-						 "{} / Keyup: {}",
-						 glfwGetKeyName(e.m_KeyCode, e.m_ScanCode), e.m_Action, g_InputComponent.m_Keyboard.m_KeyDown[e.m_KeyCode],
-						 g_InputComponent.m_Keyboard.m_KeyHeld[e.m_KeyCode], g_InputComponent.m_Keyboard.m_KeyUp[e.m_KeyCode]);
-			s_InputEventsBuffer.pop();
+			CKE_LOG_MSG(Log::Channel::Input, "KeyName: %s / Event: %d / Down: %d / Held: %d / Up: %d",
+						glfwGetKeyName(e.m_KeyCode, e.m_ScanCode), e.m_Action, g_InputComponent.m_Keyboard.m_KeyDown[e.m_KeyCode],
+						g_InputComponent.m_Keyboard.m_KeyHeld[e.m_KeyCode], g_InputComponent.m_Keyboard.m_KeyUp[e.m_KeyCode]);
 		}
 
 	} // namespace InputSystem
